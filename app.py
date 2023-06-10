fd44c2d2566740cf8d00c314017fb2934f8a699d
marusugag <74566539+marusugag@users.noreply.github.com>
2023-06-10 16:51:02 -0500

    Creacion de politicas CORS

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.monitoreo import monitoreo
 
 app = FastAPI()
 
 app.include_router(monitoreo)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
